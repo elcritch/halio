d2141019439c60997d838c15e47da495256f70d9
Jaremy Creechley <creechley@gmail.com>
2018-05-15 14:56:29 -0600

    Removing extras

--- lib/halio_protocol.ex
+++ lib/halio_protocol.ex
@@ -4,7 +4,11 @@ defprotocol HalIO do
   @spec read(struct, non_neg_integer) :: binary | {:error, term}
   def read(device, read_count)
 
-  @doc "Transfer data via a given IO hardware"
+  @doc """
+  Transfer data via a given IO hardware. The data read is equivalent to the data written.
+
+  Note: for I2C usage, use read/write separately for custom read counts.
+  """
   @spec xfer(struct, binary) :: binary | {:error, term}
   def xfer(device, value)
 
