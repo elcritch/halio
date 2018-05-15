5a5631f206e3f1e7c770f2c73a14529cb84e7315
Jaremy Creechley <creechley@gmail.com>
2018-05-15 14:32:02 -0600

    Revamping interface

--- lib/halio.ex
+++
@@ -1,8 +0,0 @@
-defprotocol HalIO do
-  @doc "read data via a given bus"
-  def read(device)
-  @doc "write data via a given bus"
-  def write(device, value)
-  @doc "transfer data via a given bus"
-  def xfer(device, value)
-end
